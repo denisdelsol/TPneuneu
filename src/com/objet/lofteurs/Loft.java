c47a327ce36ffcf0f8a661dfaf7c195f4c68d2b0
denisdelsol <Denis@.(none)>
2011-11-04 11:26:38 +0100

    Ajout de la classe Loft

---
+++ src/com/objet/lofteurs/Loft.java
@@ -0,0 +1,15 @@
+package com.objet.lofteurs;
+
+public class Loft {
+
+	private final int w;
+	private final int h;
+	private final ZoneGraphique zone;
+	
+	public Loft(int w, int h, ZoneGraphique zone)
+	{
+		this.w = w;
+		this.h = h;
+		this.zone = zone;
+	}
+}
