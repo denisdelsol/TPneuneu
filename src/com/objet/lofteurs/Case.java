e1d54f2e3592471398c159a870a903d357ad8041
denisdelsol <Denis@.(none)>
2011-11-04 12:10:43 +0100

    Ajout de la classe Case et ajout de méthodes dans Loft

---
+++ src/com/objet/lofteurs/Case.java
@@ -0,0 +1,13 @@
+package com.objet.lofteurs;
+
+public class Case {
+
+	private final int x;
+	private final int y;
+	
+	public Case(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+}
--- src/com/objet/lofteurs/Loft.java
+++ src/com/objet/lofteurs/Loft.java
@@ -6,10 +6,46 @@ public class Loft {
 	private final int h;
 	private final ZoneGraphique zone;
 	
+	/**
+	 * 
+	 * @param w largeur du loft
+	 * @param h longueur du loft
+	 * @param zone
+	 */
 	public Loft(int w, int h, ZoneGraphique zone)
 	{
 		this.w = w;
 		this.h = h;
 		this.zone = zone;
+		
+		for (int i=0; i<w;i++)
+		{
+			for (int j=0; j<h;j++)
+			{
+				//TODO Créer une case
+			}
+		}
+	}
+	
+	/**
+	 * Lance l'émission !
+	 */
+	public void go()
+	{
+		//TODO
+	}
+	
+	/**
+	 * Remplit le loft en nourriture
+	 * @param nombre
+	 */
+	public void remplissageAleatoire(float nombre)
+	{
+		//TODO
+	}
+	
+	public void add(Neuneu neuneu)
+	{
+		
 	}
 }
