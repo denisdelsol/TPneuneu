8425f74e798f32bab32dcb33c1d574ee2c58718f
denisdelsol <Denis@.(none)>
2011-11-20 14:06:59 +0100

    Ajout des méthodes addNeuneu, addNourriture et removeNeuneu

--- src/com/objet/lofteurs/Case.java
+++ src/com/objet/lofteurs/Case.java
@@ -15,9 +15,9 @@ public class Case {
 
 	private final int x;
 	private final int y;
-	private Map<Integer, Nourriture> listeNourriture; 
+	private Map<Integer, Nourriture> listeNourriture;
 	private Map<Integer, Neuneu> listeNeuneus;
-	
+
 	/**
 	 * Instancie une nouvelle instance de la classe Case.
 	 * 
@@ -28,29 +28,58 @@ public class Case {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	/**
 	 * Obtient une liste de nourriture présents sur la case.
+	 * 
 	 * @return Map<Integer, Nourriture>
 	 */
-	public Map<Integer, Nourriture> getNourriture(){
+	public Map<Integer, Nourriture> getNourriture() {
 		return this.listeNourriture;
 	}
-	
+
 	/**
 	 * Obtient une liste de neuneus présents sur la case.
-	 * @return
+	 * 
+	 * @return Map<Integer, Neuneu>
 	 */
-	public Map<Integer, Neuneu> getNeuneu(){
+	public Map<Integer, Neuneu> getNeuneu() {
 		return this.listeNeuneus;
 	}
-	
+
 	/**
+	 * Ajoute un neuneu à la case
 	 * 
 	 * @param neuneu
 	 */
-	public void addNeuneu(Neuneu neuneu){
-		
+	public void addNeuneu(Neuneu neuneu) {
+		listeNeuneus.put(listeNeuneus.size(), neuneu);
 	}
-	
+
+	/**
+	 * Ajoute un élément de nourriture à la case.
+	 * 
+	 * @param nourriture
+	 */
+	public void addNourriture(Nourriture nourriture) {
+		listeNourriture.put(listeNourriture.size(), nourriture);
+	}
+
+	/**
+	 * Enlève un neuneu d'une case.
+	 * 
+	 * @param neuneu
+	 */
+	public void removeNeuneu(Neuneu neuneu) {
+		int key = -1, i = 0;
+		while (key < 0 && i < listeNeuneus.size()) {
+			if (listeNeuneus.get(i) == neuneu) {
+				key = i;
+			}
+			i++;
+		}
+		if (key >= 0) {
+			listeNeuneus.remove(key);
+		}
+	}
 }
