1ad67a8dd8374f691a7d078de7bdb12e3e86f3ef
Bastien LEMALE <bastien.lemale@eleves.ec-nantes.fr>
2011-11-20 16:53:48 +0100

    Implémentation seDeplacer et seRepdroduire src/com/objet/lofteurs/neuneus/Cannibale.java src/com/objet/lofteurs/neuneus/Vorace.java

--- src/com/objet/lofteurs/neuneus/Cannibale.java
+++ src/com/objet/lofteurs/neuneus/Cannibale.java
@@ -2,17 +2,26 @@ package com.objet.lofteurs.neuneus;
 
 import com.objet.lofteurs.Case;
 import com.objet.lofteurs.Loft;
+import com.objet.lofteurs.Saison1;
 
 public class Cannibale extends Vorace {
 
-	public Cannibale(String name, Case caseCourrante, Loft loft) {
-		super(name, caseCourrante, loft);
+	public Cannibale(Case caseCourrante, Loft loft) {
+		super(caseCourrante, loft);
 	}
 	
 	@Override
 	public void seDeplacer(){
 		// TODO implémenter la méthode
 		return;
+	}
+	
+	@Override
+	public void seReproduire() {
+		super.copuler();
+		if (this.energie != Saison1.energieMort) {
+			this.loft.add(new Cannibale(this.caseCourrante, this.loft));
+		}
 	}
 
 }
