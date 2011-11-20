4b719defdf5c9b8949f3546eb9b762c3556a16a6
Bastien LEMALE <bastien.lemale@eleves.ec-nantes.fr>
2011-11-20 09:43:47 +0100

    Ajout méthode manger().

--- src/com/objet/lofteurs/neuneus/Neuneu.java
+++ src/com/objet/lofteurs/neuneus/Neuneu.java
@@ -1,5 +1,6 @@
 package com.objet.lofteurs.neuneus;
 
+import com.lofteur.nourriture.Nourriture;
 import com.objet.lofteurs.Case;
 import com.objet.lofteurs.Loft;
 import com.objet.lofteurs.Mangeable;
@@ -14,17 +15,36 @@ public abstract class Neuneu implements Mangeable {
 	public Neuneu(String name, Case caseDepart, Loft loft) {
 		this.name = name;
 		this.caseCourrante = caseDepart;
+		this.caseCourrante.addNeuneu(this);
 		this.loft = loft;
+		this.loft.add(this);
+		
 	}
 
 	public abstract Case seDeplacer();
 
 	public void manger() {
-		// TODO
+		final int nbNourriture = caseCourrante.getNourriture().size();
+		final int indexNourriture = (int)(Math.random()*nbNourriture);
+		
+		final Nourriture nourriture = caseCourrante.getNourriture().get(indexNourriture);
+		final int quantiteEnergie = nourriture.estMange();
+		
+		if ((energie + quantiteEnergie) < Saison1.energieDefault){
+			energie = energie + quantiteEnergie;
+		}
+		else{
+			energie = Saison1.energieDefault;
+		}
 	}
 
 	public void sereproduire() {
 		// TODO
+	}
+	
+	public int estMange(){
+		//TODO
+		return (Integer) null;
 	}
 
 }
